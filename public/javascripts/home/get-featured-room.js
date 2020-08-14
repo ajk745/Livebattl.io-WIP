ca9266c659cc99a3780cc65204ddb92cdac1fa62
ajk745 <ajk745@nyu.edu>
2020-08-14 00:12:16 -0400

    Fixed minor design error its home and rooms page

--- public/javascripts/home/get-featured-room.js
+++ public/javascripts/home/get-featured-room.js
@@ -6,12 +6,13 @@ function getFeaturedRooms() {
         if (room.namespace === '/') return;
         let li = document.createElement('li');
         li.style.background = `url(${
-          room.style.background ||
-          getComputedStyle(document.documentElement).getPropertyValue('--banner-background')
+          room.style
+            ? room.style.background || null
+            : getComputedStyle(document.documentElement).getPropertyValue('--banner-background')
         })`;
         li.style.backgroundSize = '100% 100%';
         li.innerHTML = `<h3><a href = './c${room.namespace}'>c${room.namespace}</h3></a>
-            <h6>${room.description}</h6>
+            <h6>${room.description || ''}</h6>
           <h6>Current Players: ${room.players || '0'}</h6>`;
         featuredRoomList.appendChild(li);
       });
