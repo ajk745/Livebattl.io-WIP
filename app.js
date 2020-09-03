59caee572b88a066b3a40117c91e68e99a8241e2
ajk745 <ajk745@nyu.edu>
2020-09-03 16:57:12 -0400

    Implement forever auto-restart on uncaught exception

--- app.js
+++ app.js
@@ -18,4 +18,13 @@ app.use(function (req, res, next) {
   res.sendFile(path.join(__dirname, 'public', 'not-found.html'));
 });
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res
+    .status(500)
+    .send(
+      'Seems like there was an internal error on the server. Please go back... <br><br><br> battl.live'
+    );
+});
+
 module.exports = app;
